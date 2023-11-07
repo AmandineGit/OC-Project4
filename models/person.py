a57ce9d5a5d274f0225b37266b75e31d7df9cc72
amandine <amandine.verstrepen@gmail.com>
2023-11-07 20:54:03 +0000

    Project 4 : V1.0 création des classes dans Models Création de la class Player

--- models/person.py
+++ models/person.py
@@ -1,5 +1,7 @@
 """ Define Person"""
+import datetime
 from abc import ABC
+
 
 class Person(ABC):
     """ Définition de la class Person"""
@@ -10,13 +12,25 @@ class Person(ABC):
         self.last_name = last_name
 
     def __repr__(self):
-        """ représentation de l'objet de type Person"""
+        """ représentation de l objet de type Person"""
         return self.first_name + " " + self.last_name
 
-class User(Person):
-    """ Création de la sous_classe User pour les managers et le directeur """
-    def __init__(self, first_name, last_name):
-        super().__init__(first_name, last_name)
 
-roger = User("Roger", "Rabbit")
-print(roger)
+class User(Person):
+    """ Création de la sous_classe User pour les managers et le director """
+    def __init__(self, first_name, last_name, login="", password="", enablement="0"):
+        super().__init__(first_name, last_name)
+        self.login = login
+        self.password = password
+        self.enablement = enablement
+
+
+class Player(Person):
+    """ Création de la sous_classe Player pour les players """
+    date_of_birth = datetime
+
+    def __init__(self, first_name, last_name, date_of_birth, national_chess_id=0, total_score=0):
+        super().__init__(first_name, last_name)
+        self.date_of_birth = date_of_birth
+        self.national_chess_id = national_chess_id
+        self.total_score = total_score
